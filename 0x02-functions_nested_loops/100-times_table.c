f9e6430317570172cb18fbb7c2c9dca847d49768
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-07-18 13:35:39 -0700

    Advance Task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,46 +6,51 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
-
 	if (n >= 0 && n <= 15)
 	{
+		int i, j, product;
+
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');  /* Print the first column */
+
+			for (j = 1; j <= n; j++)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+
+				product = i * j;
+				print_number(product);
 			}
+
 			_putchar('\n');
 		}
 	}
 }
 
+/**
+ * print_number - prints a number with proper formatting
+ * @num: number to print
+ */
+void print_number(int num)
+{
+	if (num < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(num + '0');
+	}
+	else if (num < 100)
+	{
+		_putchar(' ');
+		_putchar(num / 10 + '0');
+		_putchar(num % 10 + '0');
+	}
+	else
+	{
+		_putchar(num / 100 + '0');
+		_putchar((num / 10) % 10 + '0');
+		_putchar(num % 10 + '0');
+	}
+}
