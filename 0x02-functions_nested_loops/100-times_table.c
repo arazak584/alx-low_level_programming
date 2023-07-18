5de04e511cf20849a3ef964a7f57fa0faa07b602
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-07-18 13:32:48 -0700

    Advance Task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,10 +6,10 @@
  */
 void print_times_table(int n)
 {
+	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
-		int i, j, k;
-
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -21,21 +21,24 @@ void print_times_table(int n)
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar('\t');
-					_putchar('\t');
-					_putchar('\t');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
-					_putchar('\t');
-					_putchar('\t');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
 				{
-					_putchar('\t');
+					_putchar(',');
+					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
@@ -45,3 +48,4 @@ void print_times_table(int n)
 		}
 	}
 }
+
