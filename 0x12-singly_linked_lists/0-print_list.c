11a9996da13e554f193dea0478823cf219a8f0bf
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-24 13:17:42 -0700

    Task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include "lists.h"
 
-size_t print_list(const list_t *h) {
+/**
+ * Function that return the number of nodes
+ * Completed by: Abdul Razak
+ * 24-08-2023
+ */
+
+size_t print_list(const list_t *h)
+{
 	size_t count = 0;
 	const list_t *node = h;
 
 	if (node == NULL) {
-		printf("[0] (nil)\n);
+		printf("[0] (nil)\n");
 		return 0;
 	}
 
