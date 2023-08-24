dda244a7acfe08bfb7d58d600b14b06c03e01ad3
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-24 13:52:05 -0700

    Task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,13 @@
 #include "lists.h"
 
 /**
- * Prints all Elements
- * Pointer to the list_t lit to print
+ *print_list - Prints all the elements of a link list
+ * @h: pointer to the list_t lit to print
  *
- * Number of nodes printed
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
-{
+{i
 	size_t l = 0;
 
 	while (h)
