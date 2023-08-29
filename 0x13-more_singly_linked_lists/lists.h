bd9d123f8e55f5f084d4ef30a9d2618d599223b6
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-29 08:02:38 -0700

    Task

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,12 +10,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * ALX
+ * listint_s
  */
-typedef struct listint_t
+typedef struct listint_s
 {
-int n;
-struct listint_t *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
