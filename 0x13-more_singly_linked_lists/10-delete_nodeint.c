579d3ec791cef1c596c8493ea2d57d06d4ce986a
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-29 10:22:08 -0700

    Task

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,30 +7,34 @@
  *
  * Return: 1 (Success), or -1 (Fail)
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
-{
-if (head == NULL || *head == NULL || index >= listint_len(*head))
-{
-return (-1);
+int delete_nodeint_at_index(listint_t **head, unsigned int index) {
+  listint_t *current;
+  listint_t *previous;
+  unsigned int i;
+
+  if (head == NULL || *head == NULL) {
+    return (-1);
+  }
+
+  if (index == 0) {
+    *head = (*head)->next;
+    free((void*)*head);
+    return (1);
+  }
+
+  current = *head;
+  for (i = 0; current != NULL && i < index - 1; i++) {
+    previous = current;
+    current = current->next;
+  }
+
+  if (current == NULL) {
+    return (-1);
+  }
+
+  previous->next = current->next;
+  free(current);
+
+  return (1);
 }
 
-if (index == 0)
-{
-listint_t *node = *head;
-*head = node->next;
-free(node);
-return (1);
-}
-
-listint_t *current = *head;
-for (unsigned int i = 0; i < index - 1; i++)
-{
-current = current->next;
-}
-
-listint_t *node_to_delete = current->next;
-current->next = node_to_delete->next;
-free(node_to_delete);
-
-return (1);
-}
