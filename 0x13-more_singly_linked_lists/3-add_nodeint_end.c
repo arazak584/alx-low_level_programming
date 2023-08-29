290f091b92e767cc008f0dc5380d1d6441ddb7b8
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-29 06:57:51 -0700

    Task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,10 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *current = malloc(sizeof(listint_t));
+listint_t *current;
+listint_t *last_node;
+
+current = malloc(sizeof(listint_t));
 if (current == NULL)
 {
 return (NULL);
@@ -18,27 +21,18 @@ return (NULL);
 current->n = n;
 current->next = NULL;
 
-/*
- *  If the list is empty, make the new node the head
- */
 if (*head == NULL)
 {
 *head = current;
 return (current);
 }
 
-/*
- * Otherwise, find the last node in the list
- */
-listint_t *last_node = *head;
+last_node = *head;
 while (last_node->next != NULL)
 {
 last_node = last_node->next;
 }
 
-/*
- * Set the next pointer of the last node to the new node
- */
 last_node->next = current;
 
 return (current);
