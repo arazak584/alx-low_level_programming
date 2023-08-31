d3a56b539da75441ec036ebd746c8bd15be619ea
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-31 09:49:35 -0700

    Task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int n = 0;
-int i;
-if (!b)
-return (0);
+	int i;
+	unsigned int c = 0;
 
-for (i = 0; b[i]; i++)
-{
-if (b[i] < '0' || b[i] > '1')
-return (0);
-n = 2 * n + (b[i] - '0');
-}
+	if (!b)
+		return (0);
 
-return (n);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		c = 2 * c + (b[i] - '0');
+	}
+
+	return (c);
 }
