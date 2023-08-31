1798e81fc4ce95141233fc1f981a14e367f28a99
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-31 09:38:53 -0700

    Task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+/**
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
+ *
+ * Return: 1 for success, -1 for failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+{
+return (-1);
+}
+unsigned long int mask = 1 << index;
+
+*n = (*n & ~mask);
+
+return (1);
+}
