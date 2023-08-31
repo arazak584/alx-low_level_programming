5019ff90dfb7f0ff10799e3fbb2c36d7b91d07da
Abdul Razak Nuhu <arazak584@yahoo.com>
2023-08-31 09:54:47 -0700

    Task

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,20 +8,18 @@
  *
  * Return: number of bits to change
  */
-int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-  int i, c;
-  unsigned long int new;
-  unsigned long int exclusive = n ^ m;
+	int i, c = 0;
+	unsigned long int new;
+	unsigned long int exclusive = n ^ m;
 
-  for (i = 63; i >= 0; i--)
-{
-    new = exclusive >> i;
+	for (i = 63; i >= 0; i--)
+	{
+		new = exclusive >> i;
+		if (new & 1)
+			c++;
+	}
 
-    if (new & 1) {
-      c++;
-    }
-  }
-
-  return (c);
+	return (c);
 }
